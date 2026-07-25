5f04a2e346a4c4c96459b33d8620f9ec38581b95
Manoel Lima <manoelsslima@yahoo.com.br>
2026-07-25 03:06:49 -0400

    refactor: passing parameters to components and props.children

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,27 @@ import './App.css'
 
 // no react, componentes são FUNÇÕES
 
+// props é um objeto que contém todas as propriedades passadas para o componente
+// props.children é uma propriedade especial que contém os elementos filhos do componente,
+// ou seja, tudo que está entre a abertura e o fechamento do componente
+function TituloFormulario(props){
+  return (
+    <h2>{props.children}</h2>
+  )
+}
+
 function FormularioDeEvento() {
   return (
     <form className="form-evento">
-      <h2>Preencha para criar um evento:</h2>
+      <TituloFormulario>
+        Preencha para criar um evento:
+      </TituloFormulario>
       <fieldset>
         <label htmlFor="nome">Qual o nome do evento?</label>
-        <input type="text" id="nome" name="nome" />
+        <input type="text" id="nome" name="nome" placeholder="Summer dev hits" />
       </fieldset>
-      </form>
-  )
+    </form>
+  );
 }
 
 function App() {
@@ -23,10 +34,10 @@ function App() {
   return (
     <main>
       <header>
-        <img src="/logo.png" alt="Logo" />
+        <img src="/logo.png" alt="Logo do TechBoard" />
       </header>
       <section>
-        <img src="/banner.png" alt="Banner" />
+        <img src="/banner.png" alt="Banner de tecnologia" />
       </section>
       <FormularioDeEvento />
     </main>

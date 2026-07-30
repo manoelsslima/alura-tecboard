f3615eea493464d5ac1e12bd20cc31abada29f6d
Manoel Lima <manoelsslima@yahoo.com.br>
2026-07-29 23:39:20 -0400

    feat: add Tema and Banner components with styles; update App to include new components

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 import { FormularioDeEvento } from './components/FormularioDeEvento'
+import { Tema } from './components/Tema'
+import { Banner } from './components/Banner'
 
 // no react, componentes são FUNÇÕES
 
@@ -65,15 +67,58 @@ import { FormularioDeEvento } from './components/FormularioDeEvento'
 
 function App() {
 
+  const temas = [
+    {
+      id: 1,
+      nome: 'front-end'
+    },
+    {
+      id: 2,
+      nome: 'back-end'
+    },
+    {
+      id: 3,
+      nome: 'devops'
+    },
+    {
+      id: 4,
+      nome: 'inteligência artificial'
+    },
+    {
+      id: 5,
+      nome: 'data science'
+    },
+    {
+      id: 6,
+      nome: 'cloud'
+    }
+  ]
+
   return (
     <main>
       <header>
         <img src="/logo.png" alt="Logo do TechBoard" />
       </header>
-      <section>
-        <img src="/banner.png" alt="Banner de tecnologia" />
-      </section>
+      <Banner />
       <FormularioDeEvento />
+      <section>
+        <Tema tema={temas[0]} />
+      </section>
+      <section>
+        <Tema tema={temas[1]} />
+      </section>
+      <section>
+        <Tema tema={temas[2]} />
+      </section>
+      <section>
+        <Tema tema={temas[3]} />
+      </section>
+      <section>
+        <Tema tema={temas[4]} />
+      </section>
+      <section>
+        <Tema tema={temas[5]} />
+      </section>
     </main>
   )
 }
